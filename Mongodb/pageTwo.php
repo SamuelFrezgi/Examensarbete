3f949b476b3f565b1784b69c58de3765465c7169
a19samke <a19samke@student.his.se>
2022-04-11 14:37:04 +0200

    Add page Chart script Data

--- Mongodb/pageTwo.php
+++ Mongodb/pageTwo.php
@@ -13,7 +13,7 @@
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
     <link rel="stylesheet" href="../Style.css">
 
-    <script src="data_WithOutTotal.js"></script>
+    <script src="dataWithOutTotal.js"></script>
 
 </head>
 
